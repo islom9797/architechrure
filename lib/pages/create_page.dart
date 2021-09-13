ce2368a495e128d47c6929800fca90e3fadfee15
islom9797 <rixsiyevvv@gmail.com>
2021-09-14 01:25:06 +0500

    1

--- lib/pages/create_page.dart
+++ lib/pages/create_page.dart
@@ -2,6 +2,7 @@ import 'package:architechrure/model/post_model.dart';
 import 'package:architechrure/services/http_request.dart';
 import 'package:architechrure/viewmodel/createviewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CreatPage extends StatefulWidget {
 
@@ -21,52 +22,57 @@ class _CreatPageState extends State<CreatPage> {
         appBar: AppBar(
           title: Text("Creat Post"),
         ),
-        body: Stack(
-          children: [
-            Container(
-              padding: EdgeInsets.all(20),
-              child: Column(
+        body: ChangeNotifierProvider<Createviewmodel>(
+          create: (context)=>viewmodel,
+          child: Consumer<Createviewmodel>(
+            builder: (ctx,child,model)=>Stack(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
 
-                children: [
-                  Container(
-                    height: 40,
-                    width: double.infinity,
-                    child: TextField(
-                      controller: viewmodel.titleController,
-                      decoration: InputDecoration(
-                        labelText: "Title",
-                        border: OutlineInputBorder (),
+                    children: [
+                      Container(
+                        height: 40,
+                        width: double.infinity,
+                        child: TextField(
+                          controller: viewmodel.titleController,
+                          decoration: InputDecoration(
+                            labelText: "Title",
+                            border: OutlineInputBorder (),
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  SizedBox(height: 20,),
+                      SizedBox(height: 20,),
 
-                  Container(
-                    height: 100,
-                    width: double.infinity,
-                    child: TextField(
-                      controller: viewmodel.postController,
-                      decoration: InputDecoration(
-                        labelText: "Post",
-                        border: OutlineInputBorder(),
+                      Container(
+                        height: 100,
+                        width: double.infinity,
+                        child: TextField(
+                          controller: viewmodel.postController,
+                          decoration: InputDecoration(
+                            labelText: "Post",
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
                       ),
-                    ),
+                      RaisedButton(
+                        onPressed: (){
+                          viewmodel.apiCreatPost (context);
+                        },
+                        child: Text("Creat post"),
+                        color: Colors.blue,
+                      )
+                    ],
                   ),
-                  RaisedButton(
-                    onPressed: (){
-                      viewmodel.apiCreatPost (context);
-                    },
-                    child: Text("Creat post"),
-                    color: Colors.blue,
-                  )
-                ],
-              ),
+                ),
+                viewmodel.isLoading ? Center (
+                  child: CircularProgressIndicator(),
+                ) : SizedBox.shrink(),
+
+              ],
             ),
-            viewmodel.isLoading ? Center (
-              child: CircularProgressIndicator(),
-            ) : SizedBox.shrink(),
-
-          ],
+          ),
         )
     );
   }
--- lib/pages/update_page.dart
+++ lib/pages/update_page.dart
@@ -2,12 +2,13 @@ import 'package:architechrure/model/post_model.dart';
 import 'package:architechrure/services/http_request.dart';
 import 'package:architechrure/viewmodel/updateviewmdoel.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class UpdatePage extends StatefulWidget {
 
   static final String id = "update_page";
   final Post post;
- 
+
 
   UpdatePage({this.post});
 
@@ -31,48 +32,53 @@ class _UpdatePageState extends State<UpdatePage> {
         title: Text('Update post'),
       ),
       backgroundColor: Colors.white,
-      body: Stack(
-        children: [
-          Container(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              children: [
-                TextField(
-                  controller: viewmodel.titleController,
-                  decoration: InputDecoration(
-                    labelText: "Post Title",
-                    border: OutlineInputBorder(),
-                    contentPadding: EdgeInsets.only(right: 15, left: 15),
-                  ),
+      body: ChangeNotifierProvider(
+        create: (context)=>viewmodel,
+        child: Consumer(
+          builder: (ctx,child,model)=>Stack(
+            children: [
+              Container(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: viewmodel.titleController,
+                      decoration: InputDecoration(
+                        labelText: "Post Title",
+                        border: OutlineInputBorder(),
+                        contentPadding: EdgeInsets.only(right: 15, left: 15),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    TextField(
+                      controller: viewmodel.bodyController,
+                      decoration: InputDecoration(
+                        labelText: "Post Body",
+                        border: OutlineInputBorder(),
+                        contentPadding: EdgeInsets.only(
+                            right: 15, top: 15, bottom: 50, left: 15),
+                      ),
+                      maxLines: 7,
+                    ),
+                    SizedBox(height: 30,),
+                    RaisedButton(
+                      onPressed: () {
+                        viewmodel.apiCreatePost(context);
+                      },
+                      child: Text ("Update"),
+                      color: Colors.blue,
+                    ),
+                  ],
                 ),
-                SizedBox(
-                  height: 15,
-                ),
-                TextField(
-                  controller: viewmodel.bodyController,
-                  decoration: InputDecoration(
-                    labelText: "Post Body",
-                    border: OutlineInputBorder(),
-                    contentPadding: EdgeInsets.only(
-                        right: 15, top: 15, bottom: 50, left: 15),
-                  ),
-                  maxLines: 7,
-                ),
-                SizedBox(height: 30,),
-                RaisedButton(
-                  onPressed: () {
-                    viewmodel.apiCreatePost(context);
-                  },
-                  child: Text ("Update"),
-                  color: Colors.blue,
-                ),
-              ],
-            ),
-          ),
+              ),
 
-          viewmodel.isLoading ? Center(child: CircularProgressIndicator()) : SizedBox.shrink(),
-        ],
-      ),
+              viewmodel.isLoading ? Center(child: CircularProgressIndicator()) : SizedBox.shrink(),
+            ],
+          ),
+        ),
+      )
     );
   }
 }
